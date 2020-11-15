63d0e1e78208197c3384e8896f55a188bb60226c
Albert Haikou <albert_haikou@epam.com>
2020-11-15 22:19:35 +0300

    add task 5 BE part implementation

---
+++ import-service/handler.js
@@ -0,0 +1,7 @@
+import { importProductsFile } from './src/lambdas/import-products-file.ts';
+import { importFileParser } from './src/lambdas/import-file-parser.ts';
+
+export {
+  importProductsFile,
+  importFileParser
+}
